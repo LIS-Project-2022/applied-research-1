acd8d888c8f08c969bc680ed3f62216b19462cd5
XavierCanjura <36569554+XavierCanjura@users.noreply.github.com>
2022-02-13 13:05:56 -0600

    modificacion de navbar

--- dashboard/pizzas/create.php
+++ dashboard/pizzas/create.php
@@ -1,6 +1,6 @@
 <?php
     require_once("../../app/views/dashboard/template/page.class.php");
-    Page::templateHeader('Pizzas');
+    Page::templateHeader('Pizzeria SORWX');
     require_once("../../app/controllers/pizzas/create_controller.php");
     Page::templateFooter();
 ?>
